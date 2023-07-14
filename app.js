4794ffc3534447c5bd3375eb9904d0bdf54f1cff
fernandogtorrez <fernandogtorrez@gmail.com>
2023-07-13 19:11:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3030;
+
+app.use(express.static('public'));
+
+/* rutas */
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.get("/header", (req, res) =>
+  res.sendFile(path.join(__dirname, "views","partials", "header.html"))
+);
+
+
+app.listen(PORT, () =>
+  console.log("Server running in http://localhost:" + PORT)
+);
